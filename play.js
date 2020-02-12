cbfeade2470bdc7c00f67bc4cdbca0841175ad45
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-12 19:08:06 +0000

    server can now handle user input

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require('./client');
+
+const handleUserInput = (key) => { 
+  // this loops exits after you press Ctrl + C
+  if (key === '\u0003') { 
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+
+
+
 console.log('Connecting ...');
 connect();
