c93a581e9acd23ff6818fa150a6c204c987429b4
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-12 18:55:18 +0000

    make snake move continuosly by itself not manually within code

--- client.js
+++ client.js
@@ -14,9 +14,16 @@ const connect = function() {
     console.log('Successfully connected to game server');
   });
 
-  //sends my player name to server
   conn.on('connect', () => {
+  //sends my player name to server
     conn.write('Name: AAA');
+  //sends what moevemnt i want to do with the snake to the server
+    setTimeout(() => {
+      setInterval(() => {
+        conn.write('Move: up');
+      }, 50)
+    }, 700)
+  
   });
  
   return conn;
