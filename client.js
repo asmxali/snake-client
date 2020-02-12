dddac6eb446192ce5e5e54e17d48d4f67a61130d
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-12 18:36:36 +0000

    set player name

--- client.js
+++ client.js
@@ -1,15 +1,24 @@
 const net = require('net');
 
 const connect = function() {
+
   const conn = net.createConnection({ 
     host: '172.46.0.236',
     port: 50541
   });
-  // interpret incoming data as text
+ // interpret incoming data as text
   conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log(data);
+
+  //lets us know when we have successfully connected by printing to the console
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
   });
+
+  //sends my player name to server
+  conn.on('connect', () => {
+    conn.write('Name: AAA');
+  });
+ 
   return conn;
 }
 
